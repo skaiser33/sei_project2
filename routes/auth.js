12c9e2e13fe99d65a64dbe9e9703b00ff09ed91e
skaiser33 <skaiser33@gmail.com>
2021-02-24 17:31:22 -0500

    adds password update functionality

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const passport = require('../config/ppConfig')
 const db = require('../models');
+const bcrypt = require('bcrypt')
+// let currentUser
 
 router.get('/signup', (req, res) => {
   res.render('auth/signup');
@@ -45,13 +47,61 @@ router.post('/login', passport.authenticate('local', {
   successFlash: 'You have logged in!'
 }));
 
+// router.post('/changepw', passport.authenticate('local', {
+//   successRedirect: '/profile',
+//   failureRedirect: '/profile',
+//   failureFlash: 'Invalid  password',
+//   successFlash: 'You have successfully changed your password!'
+// }));
+
+// POST route - modify password
+router.post('/changepw', function (req, res) {       
+  // console.log(req.user);
+  // console.log(req.body);
+
+  const oldPassword = req.body.oldpassword;
+  const updatedPassword = req.body.updatedpassword;
+  
+
+  if (bcrypt.compareSync(oldPassword, req.user.password)) {
+    // console.log('password is correct');
+    db.user.findOne({
+      where: { id: req.user.id }
+    })
+      .then((user) => {
+    // db.user.findByPk(req.user.id)
+    // .then(function(user)
+      console.log(user.name);
+      let hash = bcrypt.hashSync(updatedPassword, 12);
+      // store the hash as the user's password in the db
+      // user.update = hash
+      user.update({
+        password: hash
+      })
+      console.log(`${hash}SUCCESS!`);;
+      
+    })
+    
+    // else {
+    // // console.log('password is incorrect');
+  };
+
+
+  // if (password !== confirm) return res.end('passwords do not match');
+  
+  // update the user db here
+
+  res.end('password reset');
+});
+
+
 router.get('/logout', (req, res) => {
   // .logut() is added to the req object by passport
   req.logout();
   // FLASH
   req.flash('success', 'You have logged out');
   res.redirect('/');
-});
+}); 
 
 //PROFILE OPTIONS
 //PUT functionality to verify and update password
