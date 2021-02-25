4503f3f6d5f26b235585893ccf9a089f561fd7fc
JacobRishe <jacob.rishe@gmail.com>
2021-02-24 23:09:54 -0800

    populated dropdown menus for comedians and topics using ejs & forEach loops

--- routes/comedian.js
+++ routes/comedian.js
@@ -9,7 +9,22 @@ router.get('/profile', isLoggedIn, (req, res) => {
   res.render('profile');
 });
 
-//GET functionality for populating joke list including laugh button, laugh count 
+//GET functionality for populating joke list including laugh button, laugh count
+//make a call to the database to display
+
+
+
+//
+// in template
+// <% comedians.forEach %>
+// <option value= results.id>results.name</option>
+// <% } %>
+
+
+// what makes up a dropdown list
+//
+// where does this data come from
+
 
 //POST functionality for adding a laugh
 
--- routes/main.js
+++ routes/main.js
@@ -8,12 +8,42 @@ const isLoggedIn = require('../middleware/isLoggedIn');
 
 //GET functionality for populating comedian dropdown)
 router.get('/', (req, res) => {
-  res.render('main')
-  })    
+  db.comedian.findAll()
+  .then((comedians) => {
+    console.log(comedians)
+      res.render('main', {allComedians: comedians})
+  })
+  .catch((error) => {
+      console.log('Error in GET /', error)
+      res.status(400).render('main/404')
+    })
+})
+//GET functionality for populating topic dropdown)
+router.get('/', (req, res) => {
+  db.topic.findAll()
+  .then((topics) =>{
+    console.log(topics)
+    res.render('main', {allTopics: topics})
+  })
+  .catch((error) => {
+      console.log('Error in GET /', error)
+      res.status(400).render('main/404')
+    })
+})
 
-//GET functionality for populating topic dropdown)  
+//
+//
+// const comedians = db.comedians.findAll();
+//
+// router.get('/comedians', (req, res) => {
+//   res.render('layout', {comedians: results})
+// })
+// .catch((error) => {
+//   console.log('Error in GET /', error)
+//   res.status(400).render('main/404')
+// })
+//POST functionality for selecting comedian
 
-//POST functionality for selecting comedian 
 
 //POST functionality for selecting topic [if these are part of the nav bar that appears on all post-login pages, this only needs to be written once]
 
