45cb99067f2bed4a0cfef00dc468cba7f3bb42b5
BernardaP <btp210@gmail.com>
2021-02-26 12:50:56 -0700

    saving local branch to merge with submain -Jacob's changes-

--- routes/comedian.js
+++ routes/comedian.js
@@ -46,6 +46,41 @@ router.get('/', (req, res) => {
 // where does this data come from
 
 
+//=== ADD LAUGH BUTTON===//
+//POST functionality for adding a laugh
+router.post('/addjoke/:id', async (req, res) => {
+  try {
+    const foundJoke = await db.joke.findByPk(req.params.id)
+    foundJoke.likes = foundJoke.likes + 1
+    foundJoke.save()
+    const foundUser = await db.user.findByPk(1)     //REMEMBER TO change to req.user.id
+    foundUser.addJoke(foundJoke)
+    console.log('===========')
+    console.log(foundUser.name, 'has faved', foundJoke.content)
+    res.redirect('/topic')
+  } catch (error) {
+    req.flash('error', error.message)
+    res.redirect('/topic')
+  }	 
+})
+
+//POST functionality for un-laugh
+router.post('/takejoke/:id', async (req, res) => {
+  try {
+    const foundJoke = await db.joke.findByPk(req.params.id)
+    foundJoke.likes = foundJoke.likes - 1
+    foundJoke.save()  
+    const foundUser = await db.user.findByPk(1)     //REMEMBER TO change to req.user.id
+    foundUser.removeJoke(foundJoke)
+    console.log('===========')
+    console.log(foundUser.name, 'has removed', foundJoke.content)
+    res.redirect('/topic') 
+  } catch (error) {
+    req.flash('error', error.message)
+    res.redirect('/topic')
+  }	 
+})
+
 //POST functionality for adding a laugh
 
 //DELETE functionality for un-laugh
