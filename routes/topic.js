77e4f8eaf4f86be5fd553f51e5bb0bee478b3563
skaiser33 <skaiser33@gmail.com>
2021-02-26 13:12:06 -0500

    adds functionality of laugh button to add user favorite

--- routes/topic.js
+++ routes/topic.js
@@ -16,7 +16,7 @@ router.get('/', (req, res) => {
       db.topic.findAll()
       .then((topics) => {
         db.topic.findOne({
-        where: {id: 2},         //REMEMBER TO change to actual topic id
+        where: {id: 3},         //REMEMBER TO change to actual topic id
         include: [db.joke]
       }).then((topic) => {
         // console.log("--------", user.dataValues.jokes[0].dataValues.content)
@@ -53,17 +53,59 @@ router.get('/', (req, res) => {
 
 //=== ADD LAUGH BUTTON===//
 //POST functionality for adding a laugh
-router.post('/addjoke/:id', function(req, res){
-  db.joke.findOne({
+// router.post('/addjoke/:id', function(req, res){
+//   db.joke.findOne({
+//     where: {id: req.params.id}
+//   }).then(function(foundJoke){
+//     // console.log("______0-----", foundJoke)
+//     foundJoke.likes = foundJoke.likes + 1
+//     // console.log("+++++++", foundJoke)
+//     foundJoke.save()
+
+//   }).then(function (foundJoke) {
+//     db.user.findOne({
+//       where: {id: 1}, //REMEMBER TO change to req.user.id
+//     include: [db.joke]
+//   }).then((user) => {
+//       user.addJoke(foundJoke).then(function(relationInfo){
+//         console.log(user.name, 'has faved', foundJoke.content)
+//         res.redirect('/topic')
+//       })
+//     })
+//   })
+// })
+
+router.post('/addjoke/:id', async (req, res) => {
+  try {
+  const foundJoke = await db.joke.findOne({
     where: {id: req.params.id}
-  }).then(function(foundJoke){
-    // console.log("______0-----", foundJoke)
-    foundJoke.likes = foundJoke.likes + 1
-    // console.log("+++++++", foundJoke)
-    foundJoke.save()
-    res.redirect('/topic')
   })
+  foundJoke.likes = foundJoke.likes + 1
+  foundJoke.save()
+
+
+  const foundUser = await db.user.findOne({
+      where: {id: 1}   //REMEMBER TO change to req.user.id
+
+  })
+      
+  foundUser.addJoke(foundJoke)
+  
+  console.log('===========')
+  console.log(foundUser.name, 'has faved', foundJoke.content)
+  res.redirect('/topic')
+      // })
+    } catch (error) {
+
+      req.flash('error', error.message)
+      res.redirect('/topic')
+
+  }	
+ 
 })
+
+
+
 
 
 //POST functionality for un-laugh
@@ -80,9 +122,62 @@ router.post('/takejoke/:id', function(req, res){
   })
 })
 
+// db.toy.findOrCreate({
+//   where: {type: "ball", color: "green"}
+// }).then(function([toy, created]) {
+//   toy.getPets().then(function(pets) {
+//     // Check if their are any pets associated with this toy
+//     if (pets.length > 0) {
+//       pets.forEach(function(pet) {
+//         console.log(pet.name, 'loves their', toy.color, toy.type);
+//       });
+//     } else {
+//       // findOrCreate a Pet and add it to the toy
+//       db.pet.findOrCreate({
+//         where: {
+//           name: "Ruby Tuesday",
+//           species: "Toy Aussie"
+//         }
+//       }).then(function([pet, created]) {
+      //   db.user.findOne({
+      //     where: {id: 1}, //REMEMBER TO change to req.user.id
+      //     include: [db.joke]
+      //   }).then((user) => {
+      //       user.addJoke(joke).then(function(relationInfo){
+      //         console.log(user.name, 'has faved', joke.content)
+      //   })
+      // });
 
 
-//DELETE functionality for un-laugh
+// POST /pokemon - receive the name of a pokemon and add it to the database
+// router.post('/', function(req, res) {
+//   // TODO: Get form data and add a new record to DB
+//   // res.send(req.body.name);
+//   db.pokemon.findOrCreate({
+//     where: {
+//       name: req.body.name
+//     }, 
+//     defaults: {}
+//     }).then(function ([foundOrCreatedPoke, created]) {    
+//       console.log(foundOrCreatedUser.firstName);
+//       console.log('---------------');
+//       console.log(created);
+//   })
+//   .finally(function() {
+//     res.redirect('/pokemon')
+//   })  
+// });
+
+// DELETE Pokemon from the favorites list.
+// router.post('/delete', function(req, res) {
+//   db.pokemon.destroy({
+//     where: { name: req.body.name }
+//   }).then(function() {
+//     console.log(req.body.name+ ' deleted');
+//     res.redirect('/pokemon')
+//   });
+// })
+
 
 
 
