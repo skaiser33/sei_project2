745079617902b30c2381c05212bcd8f4ede10644
BernardaP <btp210@gmail.com>
2021-02-25 20:54:39 -0700

    adds post route functionality for 'add laugh' button

--- routes/topic.js
+++ routes/topic.js
@@ -16,7 +16,7 @@ router.get('/', (req, res) => {
       db.topic.findAll()
       .then((topics) => {
         db.topic.findOne({
-        where: {id: 1},         //REMEMBER TO change to actual topic id
+        where: {id: 2},         //REMEMBER TO change to actual topic id
         include: [db.joke]
       }).then((topic) => {
         // console.log("--------", user.dataValues.jokes[0].dataValues.content)
@@ -30,7 +30,40 @@ router.get('/', (req, res) => {
 });
 
 
+// router.get('/:id', (req, res) => {
+//   db.comedian.findAll()
+//     .then((comedians) => {
+//       db.topic.findAll()
+//       .then((topics) => {
+//         db.topic.findOne({
+//         where: {id: req.params.id},         //REMEMBER TO change to actual topic id
+//         include: [db.joke]
+//       }).then((topic) => {
+//         // console.log("--------", user.dataValues.jokes[0].dataValues.content)
+//         res.render('topic.ejs', {topic: topic, allTopics: topics, allComedians: comedians});
+//       // }).catch((error) => {
+//       //   console.log('Error in GET /', error)
+//       //   res.status(400).render('main/404')
+//       })
+//     })  
+//   })
+// });
+
 //POST functionality for adding a laugh
+router.post('/addjoke/:id', function(req, res){
+  db.joke.findOne({
+    where: {id: req.params.id}
+  }).then(function(foundJoke){
+    // console.log("______0-----", foundJoke)
+    foundJoke.likes = foundJoke.likes + 1
+    // console.log("+++++++", foundJoke)
+    foundJoke.save()
+    res.redirect('/topic')
+  })
+})
+
+
+
 
 //DELETE functionality for un-laugh
 
