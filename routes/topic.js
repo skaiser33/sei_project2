0686dd8706c3c8aee9fe4e2a0dca8dd432e3a214
skaiser33 <skaiser33@gmail.com>
2021-02-26 13:59:43 -0500

    adds UNLIKE functionality to remove from user favs and refactors as async await

--- routes/topic.js
+++ routes/topic.js
@@ -77,109 +77,52 @@ router.get('/', (req, res) => {
 
 router.post('/addjoke/:id', async (req, res) => {
   try {
-  const foundJoke = await db.joke.findOne({
-    where: {id: req.params.id}
-  })
+  const foundJoke = await db.joke.findOne({where: {id: req.params.id}})
   foundJoke.likes = foundJoke.likes + 1
   foundJoke.save()
-
-
   const foundUser = await db.user.findOne({
       where: {id: 1}   //REMEMBER TO change to req.user.id
-
-  })
-      
+  })    
   foundUser.addJoke(foundJoke)
-  
   console.log('===========')
   console.log(foundUser.name, 'has faved', foundJoke.content)
   res.redirect('/topic')
-      // })
-    } catch (error) {
-
-      req.flash('error', error.message)
-      res.redirect('/topic')
-
-  }	
- 
+  } catch (error) {
+    req.flash('error', error.message)
+    res.redirect('/topic')
+  }	 
 })
-
-
-
-
 
 //POST functionality for un-laugh
 
-router.post('/takejoke/:id', function(req, res){
-  db.joke.findOne({
-    where: {id: req.params.id}
-  }).then(function(foundJoke){
-    // console.log("______0-----", foundJoke)
-    foundJoke.likes = foundJoke.likes - 1
-    // console.log("+++++++", foundJoke)
-    foundJoke.save()
-    res.redirect('/topic')
-  })
-})
-
-// db.toy.findOrCreate({
-//   where: {type: "ball", color: "green"}
-// }).then(function([toy, created]) {
-//   toy.getPets().then(function(pets) {
-//     // Check if their are any pets associated with this toy
-//     if (pets.length > 0) {
-//       pets.forEach(function(pet) {
-//         console.log(pet.name, 'loves their', toy.color, toy.type);
-//       });
-//     } else {
-//       // findOrCreate a Pet and add it to the toy
-//       db.pet.findOrCreate({
-//         where: {
-//           name: "Ruby Tuesday",
-//           species: "Toy Aussie"
-//         }
-//       }).then(function([pet, created]) {
-      //   db.user.findOne({
-      //     where: {id: 1}, //REMEMBER TO change to req.user.id
-      //     include: [db.joke]
-      //   }).then((user) => {
-      //       user.addJoke(joke).then(function(relationInfo){
-      //         console.log(user.name, 'has faved', joke.content)
-      //   })
-      // });
-
-
-// POST /pokemon - receive the name of a pokemon and add it to the database
-// router.post('/', function(req, res) {
-//   // TODO: Get form data and add a new record to DB
-//   // res.send(req.body.name);
-//   db.pokemon.findOrCreate({
-//     where: {
-//       name: req.body.name
-//     }, 
-//     defaults: {}
-//     }).then(function ([foundOrCreatedPoke, created]) {    
-//       console.log(foundOrCreatedUser.firstName);
-//       console.log('---------------');
-//       console.log(created);
+// router.post('/takejoke/:id', function(req, res){
+//   db.joke.findOne({
+//     where: {id: req.params.id}
+//   }).then(function(foundJoke){
+//     // console.log("______0-----", foundJoke)
+//     foundJoke.likes = foundJoke.likes - 1
+//     // console.log("+++++++", foundJoke)
+//     foundJoke.save()
+//     res.redirect('/topic')
 //   })
-//   .finally(function() {
-//     res.redirect('/pokemon')
-//   })  
-// });
-
-// DELETE Pokemon from the favorites list.
-// router.post('/delete', function(req, res) {
-//   db.pokemon.destroy({
-//     where: { name: req.body.name }
-//   }).then(function() {
-//     console.log(req.body.name+ ' deleted');
-//     res.redirect('/pokemon')
-//   });
 // })
 
-
-
-
+router.post('/takejoke/:id', async (req, res) => {
+  try {
+    const foundJoke = await db.joke.findOne({where: {id: req.params.id}})
+    foundJoke.likes = foundJoke.likes - 1
+    foundJoke.save()  
+    const foundUser = await db.user.findOne({
+        where: {id: 1}   //REMEMBER TO change to req.user.id
+    })       
+    foundUser.removeJoke(foundJoke)
+    console.log('===========')
+    console.log(foundUser.name, 'has removed', foundJoke.content)
+    res.redirect('/topic') 
+  } catch (error) {
+    req.flash('error', error.message)
+    res.redirect('/topic')
+  }	 
+})
 
 module.exports = router;
