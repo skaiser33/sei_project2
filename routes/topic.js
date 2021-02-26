d10faf9fd2b43c1f6601f275f3e2b0c0ffc507b6
skaiser33 <skaiser33@gmail.com>
2021-02-26 14:36:11 -0500

    adds temp workaround for navbar hide AND cleans up unsued code on topic and favorites js

--- routes/topic.js
+++ routes/topic.js
@@ -4,11 +4,6 @@ const passport = require('../config/ppConfig')
 const db = require('../models');
 const isLoggedIn = require('../middleware/isLoggedIn');
 
-// we use the middleware in the middle of our route to the profile (or any other page we want to restrict)
-// router.get('/profile', isLoggedIn, (req, res) => {
-//   res.render('profile');
-// });
-
 //GET functionality for populating joke list including comedian name, laugh button, laugh count
 router.get('/', (req, res) => {
   db.comedian.findAll()
@@ -30,51 +25,8 @@ router.get('/', (req, res) => {
 });
 
 
-// router.get('/:id', (req, res) => {
-//   db.comedian.findAll()
-//     .then((comedians) => {
-//       db.topic.findAll()
-//       .then((topics) => {
-//         db.topic.findOne({
-//         where: {id: req.params.id},         //REMEMBER TO change to actual topic id
-//         include: [db.joke]
-//       }).then((topic) => {
-//         // console.log("--------", user.dataValues.jokes[0].dataValues.content)
-//         res.render('topic.ejs', {topic: topic, allTopics: topics, allComedians: comedians});
-//       // }).catch((error) => {
-//       //   console.log('Error in GET /', error)
-//       //   res.status(400).render('main/404')
-//       })
-//     })  
-//   })
-// });
-
-
-
 //=== ADD LAUGH BUTTON===//
 //POST functionality for adding a laugh
-// router.post('/addjoke/:id', function(req, res){
-//   db.joke.findOne({
-//     where: {id: req.params.id}
-//   }).then(function(foundJoke){
-//     // console.log("______0-----", foundJoke)
-//     foundJoke.likes = foundJoke.likes + 1
-//     // console.log("+++++++", foundJoke)
-//     foundJoke.save()
-
-//   }).then(function (foundJoke) {
-//     db.user.findOne({
-//       where: {id: 1}, //REMEMBER TO change to req.user.id
-//     include: [db.joke]
-//   }).then((user) => {
-//       user.addJoke(foundJoke).then(function(relationInfo){
-//         console.log(user.name, 'has faved', foundJoke.content)
-//         res.redirect('/topic')
-//       })
-//     })
-//   })
-// })
-
 router.post('/addjoke/:id', async (req, res) => {
   try {
     const foundJoke = await db.joke.findByPk(req.params.id)
@@ -92,19 +44,6 @@ router.post('/addjoke/:id', async (req, res) => {
 })
 
 //POST functionality for un-laugh
-
-// router.post('/takejoke/:id', function(req, res){
-//   db.joke.findOne({
-//     where: {id: req.params.id}
-//   }).then(function(foundJoke){
-//     // console.log("______0-----", foundJoke)
-//     foundJoke.likes = foundJoke.likes - 1
-//     // console.log("+++++++", foundJoke)
-//     foundJoke.save()
-//     res.redirect('/topic')
-//   })
-// })
-
 router.post('/takejoke/:id', async (req, res) => {
   try {
     const foundJoke = await db.joke.findByPk(req.params.id)
