156f0cb078bce6554140c2f2872d552be2938536
JacobRishe <jacob.rishe@gmail.com>
2021-02-26 16:18:26 -0800

    moved div menu btn burger to exclude home button for iphone and proceeding to style nav bar

--- public/script/main.js
+++ public/script/main.js
@@ -1,8 +1,11 @@
 const menuBtn = document.querySelector('.menu-btn');
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
+  //if the menuBtn is clicked
   if (!menuOpen) {
+//add a class of open
     menuBtn.classList.add('open');
+    // menuBtn.textContent = 
     console.log(menuBtn)
     menuOpen = true;
   } else {
